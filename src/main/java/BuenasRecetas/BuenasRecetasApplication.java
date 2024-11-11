708417207ce3ff794aa4db4dba0cf023c7054b83
Victor Ortiz <victorandresortiz@gmail.com>
2024-11-10 18:51:06 -0600

    agregación de semana 7

---
+++ src/main/java/BuenasRecetas/BuenasRecetasApplication.java
@@ -0,0 +1,13 @@
+package BuenasRecetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuenasRecetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuenasRecetasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BuenasRecetas/BuenasRecetasApplicationTests.java
@@ -0,0 +1,13 @@
+package BuenasRecetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuenasRecetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
